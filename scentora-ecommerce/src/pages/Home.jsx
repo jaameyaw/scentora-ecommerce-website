53f41eacbd2313b612d43b56129b82be7c8a120f
jaameyaw <johnsonameyaw22@gmail.com>
2025-07-22 08:44:34 +0000

    Create Home.jsx

---
+++ scentora-ecommerce/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+    return (
+        <>
+            <h1>Welcome to Scentora</h1>
+            <p>Your one-stop shop for the finest fragrances.</p>
+        </>
+    )
+}
