38ac003f3d95ebef1c08c2a690842fe059eba037
jaameyaw <johnsonameyaw22@gmail.com>
2025-07-23 04:34:26 +0000

    navabar add desktopMenu

--- scentora-ecommerce/src/components/Navbar.jsx
+++ scentora-ecommerce/src/components/Navbar.jsx
@@ -10,6 +10,31 @@ export default function Navbar() {
             <Link to={"/about"}>About Us </Link>
             <Link to={"/"}>Scentora </Link>
             <Link to={"/cart"}>Cart</Link>
+    <>
+        <nav className="navbar">
+            <div className="desktopMenu desktopLinks">
+                <Link to="/shop" className="link">Buy Perfumes</Link>
+                <Link to="/shop" className="link">Exclusive</Link>
+                <Link to="/shop" className="link">Men</Link>
+                <Link to="/shop" className="link">Women</Link>
+            </div>
+
+            <div className="logo">
+                <Link to="/">
+                    <img className="logo-image"src="/images/ScentO.png" alt="Scentora Logo" />
+                </Link>
+            </div>
+
+            <div className="desktopMenu">
+                <Link to="/about" className="link">About</Link>
+                <a href="#contact" className="link">Contact</a>
+                <Link to="/cart" className="link">
+                    <button onClick={toggleCart} className="cartIconDesktop">
+                        <i className="fa-solid fa-cart-shopping"></i>
+                    </button>
+                </Link>
+                <i className="fas fa-user userIcon"></i>
+            </div>
         </nav>
     )
 }
