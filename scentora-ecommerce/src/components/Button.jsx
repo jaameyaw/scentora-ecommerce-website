dfa23bd0a713e9b331dbe9ac5e8fb467c533ddc1
jaameyaw <johnsonameyaw22@gmail.com>
2025-07-25 11:30:55 +0000

    Create Button.jsx
    pass className, onclick, label props

---
+++ scentora-ecommerce/src/components/Button.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Button ({className, onClick, label }) {
+    return (
+        <button className={className} onClick={onClick}>
+            {label}
+        </button>    
+    )
+}
