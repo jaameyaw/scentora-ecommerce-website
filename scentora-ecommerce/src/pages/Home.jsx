55db4dcb8dcff054f82a843bd94c39e1aa583fc4
jaameyaw <johnsonameyaw22@gmail.com>
2025-07-25 02:18:05 +0000

    add Hero component

--- scentora-ecommerce/src/pages/Home.jsx
+++ scentora-ecommerce/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 
+import Hero from '../components/Hero';
+
 export default function Home() {
     return (
         <>
-            <h1>Welcome to Scentora</h1>
-            <p>Your one-stop shop for the finest fragrances.</p>
+            <Hero />
         </>
     )
 }
