8c325db07bffba0073492a858fd9ca3cdeefc2a1
jaameyaw <johnsonameyaw22@gmail.com>
2025-07-28 21:28:27 +0000

    Add PerfumeofTheWeek component to Home page

--- scentora-ecommerce/src/pages/Home.jsx
+++ scentora-ecommerce/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 
 import Hero from '../components/Hero';
+import PerfumeofTheWeek from '../components/PerfumeofTheWeek';
 
 export default function Home() {
     return (
         <>
             <Hero />
+            <PerfumeofTheWeek />
         </>
     )
 }
