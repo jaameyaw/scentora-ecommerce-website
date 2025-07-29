b53ffe5d2a29b0de8da09dd368c3bfb30f6166fe
jaameyaw <johnsonameyaw22@gmail.com>
2025-07-29 21:54:01 +0000

    Add category filtering and UI to Shop page
    Shop page now supports filtering perfumes by category using URL params. Added basic breadcrumb navigation, sorting dropdown, and imported perfumes data for display. Initial UI structure and styles are included.

--- scentora-ecommerce/src/pages/Shop.jsx
+++ scentora-ecommerce/src/pages/Shop.jsx
@@ -1,9 +1,33 @@
 
+import { Link, useParams} from 'react-router-dom';
+import './Shop.css';
+import perfumes from '../perfumes'; 
+
+
+
 export default function Shop() {
+    const { category } = useParams();
+    const displayedPerfumes = category ? perfumes.filter(perfume => perfume.category === category) : perfumes;
     return (
-        <>
-            <h1>Shop</h1>
-        </>
+        <section className="shop">
+            <div className="shop-header">
+                <p className="breadrumb">
+                    <Link to="/">Home</Link> / 
+                    <Link to="/shop">Shop</Link> 
+                    {category && <> / <span>{category.charAt(0).toUpperCase() + category.slice(1)}</span></>}
+                </p>
+
+                <h1 className="shop-title">SHOP</h1>
+            </div>
+
+            <div className="shop-controls">
+                <p className="filter-label">☰ Filter Products</p>
+                <select className='short-select'>
+                    <option>Default sorting</option>
+                    <option>Price: Low to High</option>
+                    <option>Price: High to Low</option>
+                </select>
+            </div>
+
     )
 
-}
