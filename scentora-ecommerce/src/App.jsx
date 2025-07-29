239b2da9055f55ba5b9e95af3aca1598629ec529
jaameyaw <johnsonameyaw22@gmail.com>
2025-07-29 21:52:27 +0000

    Add category route to Shop component
    Introduces a dynamic route '/shop/:category' to render the Shop component for specific categories, enabling category-based product views.

--- scentora-ecommerce/src/App.jsx
+++ scentora-ecommerce/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/shop/:category" element={<Shop />} />
         <Route path="/about" element={<About />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
