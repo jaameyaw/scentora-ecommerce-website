d307ae8fd9bb91b3f5ea01fa0e7c3912742c2e6d
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-01 19:02:14 +0000

    no longer using modal component
    change the modal component to earlier  two different sidebars (mobile and cart);

--- scentora-ecommerce/src/components/Navbar.jsx
+++ scentora-ecommerce/src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import "./Navbar.css";
 import '../App.css';
-import Modal from "./Modal";
 
 export default function Navbar() {
-    const [activeModal, setActiveModal] = useState(null);
-    const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState(false);
+    const [cartOpen, setCartOpen] = useState(false);
+    const toggleSidebar = () => setIsOpen(!isOpen);
+    const toggleCart = () => setCartOpen(!cartOpen);
 
-    const openModal = (type) => setActiveModal(type);
-    const closeModal = () => setActiveModal(null);
+    const navigate = useNavigate();
     return (
     <>
         <nav className="navbar">
@@ -29,66 +29,60 @@ export default function Navbar() {
             <div className="desktopMenu">
                 <Link to="/about" className="link">About</Link>
                 <a href="#contact" className="link">Contact</a>
-                <button onClick={() => openModal('cart')} >
-                    <i className="fa-solid fa-cart-shopping"></i>
-                </button>
+                    <button onClick={toggleCart} className="cartIconDesktop">
+                        <i className="fa-solid fa-cart-shopping"></i>
+                    </button>
                 <i className="fas fa-user userIcon"></i>
             </div>
 
 
             <div className="navbarMobileActions">
-                <button onClick={() => openModal('cart')}>
+                <button onClick={toggleCart} className="cartIconMobile">
                     <i className="fa-solid fa-cart-shopping"></i>
                 </button>
 
-                <button onClick={() => openModal('sidebar')} className="hamburger">
+                <button onClick={toggleSidebar} className="hamburger">
                     <i className='fas fa-bars'></i>
                 </button>
             </div>
 
         </nav>
 
+        <div className={`mobileSidebar ${isOpen ? "sidebarOpen" : ''}`}>
+            <button onClick={toggleSidebar} className="closeSidebarBtn">
+                <i className="fas fa-times "></i>
+            </button>
+            <i className={`fas fa-user userIcon`}></i>
+            <Link to="/shop" className="link" onClick={toggleSidebar}>Buy Perfumes</Link>
+            <Link to="/shop/men" className="link" onClick={toggleSidebar}>Men</Link>
+            <Link to="/shop/women" className="link" onClick={toggleSidebar}>Women</Link>
+            <Link to="/shop/unisex" className="link" onClick={toggleSidebar}>Unisex</Link>
+            <Link to="/shop" className="link" onClick={toggleSidebar}>Scent of the Month</Link>
+            <Link to="/about" className="link" onClick={toggleSidebar}>About</Link>
+            <a href="#contact" className="link" onClick={toggleSidebar}>Contact</a>
+        </div>
 
-        <Modal
-            isOpen={activeModal === 'sidebar'}
-            onClose={closeModal}
-            position="left"
-        >
-            <div className="modalSidebarContent">
-                <i className={`fas fa-user userIcon`}></i>
-                <Link to="/shop" className="link" onClick={closeModal}>Buy Perfumes</Link>
-                <Link to="/shop" className="link" onClick={closeModal}>Exclusive</Link>
-                <Link to="/shop" className="link" onClick={closeModal}>Men</Link>
-                <Link to="/shop" className="link" onClick={closeModal}>Women</Link>
-                <Link to="/shop" className="link" onClick={closeModal}>Scent of the Month</Link>
-                <Link to="/about" className="link" onClick={closeModal}>About</Link>
-                <a href="#contact" className="link" onClick={closeModal}>Contact</a>
-            </div>
-        
-        </Modal>
-
-
-        <Modal
-            isOpen={activeModal === 'cart'}
-            onClose={closeModal}
-            position="right"
-        >        
-            <div className='cartSidebarContent'>
-                <div>
-                    <div className="cartSidebarHeader">
-                        <h3><i className="fas fa-shopping-bag"></i> CART</h3>
-                    </div>
-
-                    <div className="cartSidebarItems">
-                        <p className="cartSidebarEmptyText">Your cart is empty</p>
-
-                        <button className="startShoppingBtn" onClick={() => { closeModal(); navigate('/shop'); }}>
-                            START SHOPPING
-                        </button>                    
-                    </div>   
+        <div className={`cartSidebar ${cartOpen ? "cartOpen" : ''}`}>
+            <div>
+                <div className="cartSidebarHeader">
+                    <h3><i className="fas fa-shopping-bag"></i> CART</h3>
+                    <button onClick={toggleCart} className="cartSidebarCloseBtn">
+                        <i className="fas fa-times"></i>
+                    </button>
                 </div>
+
+                <div className="cartSidebarItems">
+                    <p className="cartSidebarEmptyText">Your cart is empty</p>
+
+                    <button className="startShoppingBtn" onClick={() => { toggleCart(); navigate('/shop'); }}>
+                        START SHOPPING
+                    </button>                    
+                </div>   
+                
             </div>
-        </Modal>
+
+
+        </div>
     </>
     )
 }
