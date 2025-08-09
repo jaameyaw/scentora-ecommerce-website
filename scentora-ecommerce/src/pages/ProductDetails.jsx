36263607cff2f0445d41041d35881dd22d93f65d
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-09 23:43:34 +0000

    import mode of payment image into productDetails.jsx

--- scentora-ecommerce/src/pages/ProductDetails.jsx
+++ scentora-ecommerce/src/pages/ProductDetails.jsx
@@ -1,8 +1,12 @@
 
 import { useParams } from "react-router-dom";
 import perfumes from "../perfumes";
-
 import './ProductDetails.css';
+import mastercard from '../assets/payment-icons/visa.png';
+import paypal from '../assets/payment-icons/paypal.png';
+import discover from '../assets/payment-icons/discover.png';
+import americanExpress from '../assets/payment-icons/american-express.png';
+import mtnMomo from '../assets/payment-icons/mtn-momo.png'
 
 export default function ProductDetails() {
     const { slug } = useParams()
