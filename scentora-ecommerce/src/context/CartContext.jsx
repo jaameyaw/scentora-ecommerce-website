f5133f9585da913a57ba05f439e4f15623ba0d1d
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-13 22:32:42 +0500

    add CartContext with add, remove, and update quantity logic

---
+++ scentora-ecommerce/src/context/CartContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext(null);
+
+export function CartProvider ({children}) {
+
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (product) => {
+        setCart((prevCart) => {
+            const existingProduct = prevCart.find((cartProduct) => cartProduct.id === product.id);
+            if (existingProduct) {
+                return prevCart.map((cartProduct) =>
+                    cartProduct.id === product.id
+                    ? {...cartProduct, quantity: cartProduct.quantity + 1} 
+                    : cartProduct   
+                ); 
+            } else {
+                return [...prevCart, { ...product, quantity: 1 }]; 
+            }
+        })
+    }
+
+
+    const removeFromCart = (id) => {
+        setCart((prevCart) => prevCart.filter((cartProduct) => cartProduct.id !== id));
+    }
+
+    const updateQuantity = (id, quantity) => {
+        setCart((prevCart) =>
+        prevCart.map((cartProduct) =>
+            cartProduct.id === id ? { ...cartProduct, quantity } : cartProduct)
+        )
+    }
+
+
+    const value = { cart, setCart, addToCart, removeFromCart, updateQuantity}
+
+    return (
+        <CartContext.Provider value={value}>
+            {children}
+        </CartContext.Provider>
+    )
+}
