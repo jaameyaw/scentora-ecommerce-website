d71920fa47f33b13563d9dbe13e57d28b626ca55
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-15 23:58:21 +0000

    Render cart items with remove and quantity update functionality

--- scentora-ecommerce/src/pages/Cart.jsx
+++ scentora-ecommerce/src/pages/Cart.jsx
@@ -19,5 +19,29 @@ const Cart = () => {
                     <img src="/images/shopping-bag.png" />
                     <h2>Your Cart ({cartCount} items)</h2>
                 </div>
+
+                <div className="card-product-container">
+                    <div className="card-products">
+                        {cart.length === 0 ? (
+                            <p>Your cart is empty.</p>
+                        ): (
+                            cart.map((item) => (
+                                <div key={item.id} className="cart-item">
+                                    <button className="close-icon" onClick={() => removeFromCart(item.id)}>
+                                        <i class="fa-regular fa-trash-can"></i>
+                                    </button>
+                                    <img src={item.image} alt={item.name} className="cart-item-img" />
+                                    <div className="cart-item-details">
+                                        <h3>{item.name}</h3>
+
+                                        <div className="cart-quantity-wrapper">
+                                            <h4>Quantity</h4>
+                                            <div className="cart-quantity">
+                                                <button onClick={() => updateQuantity(item.id, item.quantity - 1)}>-</button>
+                                                <span>{item.quantity}</span>
+                                                <button onClick={() => updateQuantity(item.id, item.quantity + 1)}>+</button>
+                                            </div>
+                                        </div>
+                                    </div>
     )
 }
