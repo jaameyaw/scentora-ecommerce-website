f0db2aa7a60fff29eff500712409c8d053dd88b6
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-15 22:44:03 +0000

    Import CartContext, Breadcrumbs, and cart css in Cart component

--- scentora-ecommerce/src/pages/Cart.jsx
+++ scentora-ecommerce/src/pages/Cart.jsx
@@ -1,3 +1,9 @@
+import { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import './Cart.css';
+
 
 export default function Cart() {
     return (
