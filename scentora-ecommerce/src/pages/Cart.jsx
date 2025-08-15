be2e8ce7cab7e5e39c6de6b4fd6ca3e8146350b3
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-15 23:57:24 +0000

    Add cart header with icon and item count

--- scentora-ecommerce/src/pages/Cart.jsx
+++ scentora-ecommerce/src/pages/Cart.jsx
@@ -13,5 +13,11 @@ const Cart = () => {
                 <Link href="/" underline="hover" color='inherit'>Home</Link>
                 <Link href="/cart" underline='hover' color='inherit'>Cart</Link>
             </Breadcrumbs>
+ 
+            <div className="cart-container">
+                <div className="cart-header">
+                    <img src="/images/shopping-bag.png" />
+                    <h2>Your Cart ({cartCount} items)</h2>
+                </div>
     )
 }
