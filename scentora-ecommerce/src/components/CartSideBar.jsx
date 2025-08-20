14daf9e687bd2defbac46434e6a8ff6fed1db9fa
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-20 13:16:06 +0000

    Enhance CartSideBar with empty cart and summary UI
    CartSideBar now displays a message and navigation button when the cart is empty. When items are present, it shows a subtotal, checkout button, and a view cart button, improving user experience and navigation.

--- scentora-ecommerce/src/components/CartSideBar.jsx
+++ scentora-ecommerce/src/components/CartSideBar.jsx
@@ -1,27 +1,74 @@
 import { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
+import { useNavigate } from 'react-router-dom';
 import SideBarWrapper from './SideBarWrapper';
+import '../App.css';
+import Button from './Button';
 
 
 
 const CartSideBar = ({isOpen, onClose, children}) => {
-    const { cartCount } = useContext(CartContext);
+    const { cart, cartCount, cartTotal } = useContext(CartContext);
+    const navigate = useNavigate();
 
     return (
         <>
-            <SideBarWrapper 
-                isOpen={isOpen} 
-                onClose={onClose} 
-                position="right" 
-                variant='cart'
-                className="cartsidebarHeader" 
-                headerContent= {`Your Cart (${cartCount} items)`}
-            >
-            
-            {children}    
+            {cart.length === 0 ? (
+                <SideBarWrapper 
+                    isOpen={isOpen} 
+                    onClose={onClose} 
+                    position="right" 
+                    variant='cart'
+                    className="cartsidebarHeader" 
+                    headerContent= {`Your Cart (${cartCount} items)`}
+                >
+                
+                <div className="emptyCartSidebar">     
+                    <p className='emp'>Your cart is currently empty.</p>
+                    <Button className='ButtonTwo width ' onClick={() => {
+                        onClose();
+                        navigate('/shop');
+                    }}>
+                        Return to Shop
+                    </Button>  
+                </div>
 
-            </SideBarWrapper>   
-        
+                </SideBarWrapper>   
+            ) : (   
+                <SideBarWrapper 
+                    isOpen={isOpen} 
+                    onClose={onClose} 
+                    position="right" 
+                    variant='cart'
+                    className="cartsidebarHeader" 
+                    headerContent= {`Your Cart (${cartCount} items)`}
+                >
+                
+                <div className="sidebarContent">
+                    {children} 
+                </div>
+    
+
+                <div className="sidebarSummary">
+                    <div className="summary-row">             
+                        <span>SubTotal</span>
+                        <span className='cart-summary-value' >${cartTotal.toFixed(2)}</span>
+                    </div>
+
+                    <Button className='ButtonTwo width'>
+                        Proceed to Checkout    
+                    </Button>
+
+                    <Button className='ButtonOne width' onClick={() => {
+                        onClose();
+                        navigate('/cart');
+                    }}>
+                        view cart   
+                    </Button>
+                </div>
+
+                </SideBarWrapper>   
+            )}
         </>
     )
 }
