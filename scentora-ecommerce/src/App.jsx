702afabf43ddf08770f9ce339e00d09605fe8052
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-20 13:04:57 +0000

    Add cart sidebar component and global sidebar state management to App
    Introduces a cart sidebar using CartSideBar and CartContent components. Adds state management globally for opening and closing the sidebar, and passes toggleCart to Navbar for triggering the sidebar.

--- scentora-ecommerce/src/App.jsx
+++ scentora-ecommerce/src/App.jsx
@@ -1,27 +1,37 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import About from './pages/About';
 import Cart from './pages/Cart';
 import ProductDetails from './pages/ProductDetails';
+import CartSideBar from './components/CartSideBar';
+import CartContent from './components/CartContent';
 
 function App() {
 
+  const [cartOpen, setCartOpen] = useState(false);
+  const toggleCart = () => setCartOpen(!cartOpen);    
+  const onClose = () => setCartOpen(false);
   return (
     <Router>
-      <Navbar />
+      <Navbar  toggleCart={toggleCart}/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/shop/:category" element={<Shop />} />
         <Route path="/about" element={<About />} />
-        <Route path="/product/:slug" element={<ProductDetails />} />
+        <Route path="/product/:slug" element={<ProductDetails toggleCart={toggleCart} />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
+
+      <CartSideBar isOpen={cartOpen} onClose={onClose}>
+        <CartContent variant='sidebar' onClose={onClose} showSummary = {false}/>
+      </CartSideBar>
     </Router>
-  )
+  ) 
 }
 
 export default App
