dc42f64ecb59021dfc5610c7c910fa2adf16b67a
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-22 22:35:18 +0000

    change position/arrangement of components on Home page
    it has this arrangement now
    shop by category>why shop with us>Best Sellers

--- scentora-ecommerce/src/pages/Home.jsx
+++ scentora-ecommerce/src/pages/Home.jsx
@@ -69,66 +69,67 @@ export default function Home() {
             <Hero />
             <PerfumeofTheWeek />
             <SectionWrapper>
-            <SectionTitle subtitle="New perfumes" title="Shop by category" />
-            <div className="categories-grid">
-                {categories.map((category, index) => (
-                <CategoryCard
-                    key={index}
-                    image={category.image}
-                    title={category.title}
-                />
-                ))}
-            </div>
-            </SectionWrapper>
-
-            <SectionWrapper>
-            <SectionTitle subtitle="Check out our" title="best sellers" />
-            <div className="categories-grid">
-            {bestsellers.map((perfume, id) => (
-                <div className="product-card" key={id} onClick={() => navigate(`/product/${perfume.slug}`)}>
-                {perfume.salePrice && <span className="sale-badge">Sale!</span>}
-                <img src={perfume.image} alt={perfume.name} />
-                <p className="product-category">{perfume.category}</p>
-                <h3 className="product-name">{perfume.name}</h3>
-                <div className="product-rating">
-                    {Array(5)
-                    .fill()
-                    .map((_, i) => (
-                    <i key={i} className="fa-regular fa-star star-icon"></i>
+                <SectionTitle subtitle="New perfumes" title="Shop by category" />
+                <div className="categories-grid">
+                    {categories.map((category, index) => (
+                    <CategoryCard
+                        key={index}
+                        image={category.image}
+                        title={category.title}
+                    />
                     ))}
                 </div>
-                <div className="price">
-                    {perfume.salePrice ? (
-                    <>
-                        <span className="old-price">${perfume.price.toFixed(2)}</span>
-                        <span className="new-price">${perfume.salePrice.toFixed(2)}</span>
-                    </>
-                    ) : (
-                    <span className="new-price">${perfume.price.toFixed(2)}</span>
-                    )}
-                    
-                </div>
-                </div>
-            ))}
-            </div>
             </SectionWrapper>
 
             <SectionWrapper variant="whyshop"> 
-            <SectionTitle subtitle="why shop with" title="scentora" />
-            <div className="whyshop-grid">
-                {whyshopItems.map((item) => (
-                <div key={item.id} className="whyshop-card">
-                    <div className="whyshop-icon">
-                        <img src={item.icon} alt={item.title} />
+                <SectionTitle subtitle="why shop with" title="scentora" />
+                <div className="whyshop-grid">
+                    {whyshopItems.map((item) => (
+                    <div key={item.id} className="whyshop-card">
+                        <div className="whyshop-icon">
+                            <img src={item.icon} alt={item.title} />
+                        </div>
+                        
+                        <div className="whyshop-text">
+                            <h3>{item.title}</h3>
+                            <p>{item.description}</p>
+                        </div>
                     </div>
-                    
-                    <div className="whyshop-text">
-                        <h3>{item.title}</h3>
-                        <p>{item.description}</p>
-                    </div>
+                    ))}
                 </div>
+            </SectionWrapper>
+
+
+            <SectionWrapper>
+            <SectionTitle subtitle="Check out our" title="best sellers" />
+                <div className="categories-grid">
+                {bestsellers.map((perfume, id) => (
+                    <div className="product-card" key={id} onClick={() => navigate(`/product/${perfume.slug}`)}>
+                        {perfume.salePrice && <span className="sale-badge">Sale!</span>}
+                        <img src={perfume.image} alt={perfume.name} />
+                        <p className="product-category">{perfume.category}</p>
+                        <h3 className="product-name">{perfume.name}</h3>
+                        <div className="product-rating">
+                            {Array(5)
+                            .fill()
+                            .map((_, i) => (
+                            <i key={i} className="fa-regular fa-star star-icon"></i>
+                            ))}
+                        </div>
+                        <div className="price">
+                            {perfume.salePrice ? (
+                            <>
+                                <span className="old-price">${perfume.price.toFixed(2)}</span>
+                                <span className="new-price">${perfume.salePrice.toFixed(2)}</span>
+                            </>
+                            ) : (
+                            <span className="new-price">${perfume.price.toFixed(2)}</span>
+                            )}
+                            
+                        </div>
+                    </div>
                 ))}
-            </div>
+                </div>
             </SectionWrapper>
         </>
         )
