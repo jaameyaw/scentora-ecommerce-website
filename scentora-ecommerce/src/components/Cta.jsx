a8737db830068042f3eb9b105357960396221d63
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-23 23:03:24 +0000

    group images for cta section into an array

---
+++ scentora-ecommerce/src/components/Cta.jsx
@@ -0,0 +1,8 @@
+const images = [
+  { src: "/images/perfumesGallery/perfume1.jpg" },
+  { src: "/images/perfumesGallery/perfume2.jpg" },
+  { src: "/images/perfumesGallery/perfume3.jpg" },
+  { src: "/images/perfumesGallery/perfume4.jpg" },
+  { src: "/images/perfumesGallery/perfume5.jpg" },
+  { src: "/images/perfumesGallery/perfume6.jpg" },
+];
