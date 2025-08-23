68358dfaddd6e984f3135ff1fb34ce96c49892f7
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-23 22:45:12 +0000

    add testimonial data array to testimonialCarousel component

---
+++ scentora-ecommerce/src/components/TestimonialCarousel.jsx
@@ -0,0 +1,33 @@
+const testimonials = [
+
+    {
+        name: "Pedri Gonzalez",
+        role: "Assistant Backend Developer",
+        text: "Scentora has completely transformed my fragrance experience—quality scents and fast delivery.",
+        image: "/images/testimonial.jpg",
+        stars: 5,
+    },
+    {
+        name: "Johnson Ameyaw",
+        role: "Perfume Enthusiast",
+        text: "The scents are authentic and long-lasting. Shopping here has been such a great experience.",
+        image: "/images/testimonial.jpg",
+        stars: 4,
+    },
+    {
+
+        name: "Aitana Bonmati",
+        role: "Pyschologist",
+        text: "Amazing service and elegant perfumes. Highly recommended to anyone looking for luxury fragrances.",
+        image: "/images/testimonial.jpg",
+        stars: 5,
+    },
+
+    {
+        name: "Lamine Yamal",
+        role: "Professional Footballer",   
+        text: "I love the variety of perfumes available. Each one has a unique scent that lasts all day.",
+        image: "/images/testimonial.jpg",
+        stars: 4,
+    },
+];
