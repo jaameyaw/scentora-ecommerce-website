0e7ff01942c5a892d053c6288519b876f03c4ff5
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-24 00:32:45 +0000

    update testimonial role and image

--- scentora-ecommerce/src/components/TestimonialCarousel.jsx
+++ scentora-ecommerce/src/components/TestimonialCarousel.jsx
@@ -8,14 +8,14 @@ const testimonials = [
 
     {
         name: "Pedri Gonzalez",
-        role: "Assistant Backend Developer",
+        role: "Maestro Midfielder",
         text: "Scentora has completely transformed my fragrance experience—quality scents and fast delivery.",
-        image: "/images/testimonial.jpg",
+        image: "/images/pedri.jpg",
         stars: 5,
     },
     {
         name: "Johnson Ameyaw",
-        role: "Perfume Enthusiast",
+        role: "Frontend Developer",
         text: "The scents are authentic and long-lasting. Shopping here has been such a great experience.",
         image: "/images/testimonial.jpg",
         stars: 4,
@@ -25,7 +25,7 @@ const testimonials = [
         name: "Aitana Bonmati",
         role: "Pyschologist",
         text: "Amazing service and elegant perfumes. Highly recommended to anyone looking for luxury fragrances.",
-        image: "/images/testimonial.jpg",
+        image: "/images/aitana.jpg",
         stars: 5,
     },
 
@@ -33,7 +33,7 @@ const testimonials = [
         name: "Lamine Yamal",
         role: "Professional Footballer",   
         text: "I love the variety of perfumes available. Each one has a unique scent that lasts all day.",
-        image: "/images/testimonial.jpg",
+        image: "/images/lamine.jpg",
         stars: 4,
     },
 ];
