df95ea654d39599d5cc114f8f6ce17f70529809a
jaameyaw <johnsonameyaw22@gmail.com>
2025-08-24 16:03:44 +0000

    add newsletter signup component
    - Created Newsletter component with form and CTA button
    - Integrated styled input field for email subscription
    - Added heading, description, and promotional text
    - Prepared component for future backend integration

---
+++ scentora-ecommerce/src/components/Newsletter.jsx
@@ -0,0 +1,31 @@
+
+import "./Newsletter.css";
+import Button from './Button';
+
+const Newsletter = () => {
+  return (
+    <div className="newsletter-content">
+        <div className="newsletter-info">
+            <h2> Sign up for the <span>latest Deals &gt;</span></h2>
+            <p>
+                Get <strong>$5 off</strong> your first order of <strong>$100+</strong>{" "}
+                by signing up for our Newsletter. Be the first to receive exclusive
+                news and offers.
+            </p>
+        </div>
+
+        <div>
+            <form className="newsletter-form">
+                <input
+                    type="email"
+                    placeholder="Your email address"
+                    required
+                />
+                <Button className='ButtonTwo'>Subscribe</Button>
+            </form>
+        </div>
+    </div>
+  );
+};
+
+export default Newsletter;
