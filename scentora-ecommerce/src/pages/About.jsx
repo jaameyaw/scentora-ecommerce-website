0e0bf0363cb8a910f369ca0780d9afdaa3b65b22
jaameyaw <johnsonameyaw22@gmail.com>
2025-09-04 23:19:37 +0000

    organize About page imports
    - Imported AboutInfo component for reusable about sections
    - Added Breadcrumb and Button components for navigation and actions
    - Included CSS for About and PerfumeOfTheWeek styles
    - Imported assets (about-img, our-story.jpg) for About page visuals
    - Set up react-router-dom useNavigate for routing functionality

--- scentora-ecommerce/src/pages/About.jsx
+++ scentora-ecommerce/src/pages/About.jsx
@@ -1,3 +1,13 @@
+import './About.css';
+import Breadcrumb from '../components/Breadcrumb';
+import AboutInfo from '../components/AboutInfo';
+import '../components/PerfumeofTheWeek.css';
+import { useNavigate } from 'react-router-dom';
+import aboutImg from '../assets/about/about-img.jpg';
+import storyImg from '../assets/about/our-story.jpg';
+import Button from '../components/Button';
+
+
 
 export default function About() {
     return (
