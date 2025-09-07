8657554fac1631780468e99d564fb785a107952c
jaameyaw <johnsonameyaw22@gmail.com>
2025-09-07 07:33:19 +0000

    feat: add contact details data structure
    Added contactDetails array to Contact component
    
    Defined objects for Email, Phone, WhatsApp, and Location
    
    Included icons, display text, and action links for each contact method

--- scentora-ecommerce/src/pages/Contact.jsx
+++ scentora-ecommerce/src/pages/Contact.jsx
@@ -5,3 +5,34 @@ import Button from "../components/Button";
 
 
 
+export default function Contact() {
+
+    const contactDetails = [
+        {
+            type: "Email",
+            icon: "fa-regular fa-envelope",
+            info: "scentorafragrance@gmail.com",
+            link: "mailto:johnson11ameyaw@gmail.com"
+        }, 
+
+        {
+            type: "Phone",
+            icon: "fa-solid fa-phone",
+            info: "0242142297",
+            link: "tel:+233242142297"
+        }, 
+
+        {
+            type: "WhatsApp",
+            icon: "fa-brands fa-whatsapp",
+            info: "Chat on WhatsApp",
+            link: "https://wa.me/233242142297"
+        }, 
+
+        {
+            type: "Location",
+            icon: "fa-solid fa-map-marker-alt",
+            info: "KNUST, Kumasi, Ghana",
+            link: null
+        }, 
+    ]
